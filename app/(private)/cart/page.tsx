914ca40d71dd8fa57d86ada7249e4f8933a87919
aayushi sharma <aayushisharma@bitcot.com>
2025-12-04 17:30:52 +0530

    fixed issue

--- app/(private)/cart/page.tsx
+++ app/(private)/cart/page.tsx
@@ -1,9 +1,16 @@
 import Cart from "@/components/cart"
 import { fetchCart } from "@/store/cart/action"
 import { CartData } from "@/types/cart"
+import { redirect } from "next/navigation"
+import { PUBLIC_PATH } from "@/utils/constant"
 
 export default async function CartPage() {
   const res = await fetchCart()
+  
+  // If user is not authenticated, redirect to login instead of showing empty cart
+  if (!res.success && res.error === "UNAUTHORIZED") {
+    redirect(PUBLIC_PATH.LOGIN)
+  }
   
   const cartData: CartData = res.success && res.data ? {
     items: res.data.items || [],
